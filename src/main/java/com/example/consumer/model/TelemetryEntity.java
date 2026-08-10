a55daf6d9ec181d2069ef2039dd9d4228499f18d
100370855-gd <312981767+100370855-gd@users.noreply.github.com>
2026-08-10 16:36:43 +0200

    use lombok to auto-generate getters setters

--- src/main/java/com/example/consumer/model/TelemetryEntity.java
+++ src/main/java/com/example/consumer/model/TelemetryEntity.java
@@ -1,15 +1,18 @@
 package com.example.consumer.model;
 
 import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
 /**
  * JPA entity representing telemetry data persisted in the database.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "telemetry")
 public class TelemetryEntity {
@@ -32,57 +35,6 @@ public class TelemetryEntity {
 
     @Column(nullable = false)
     private Instant timestamp;
-
-    public TelemetryEntity() {
-    }
-
-    /**
-     * Gets the primary key ID of the telemetry entity.
-     * @return the ID
-     */
-    public Long getId() {
-        return id;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(String deviceId) {
-        this.deviceId = deviceId;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    public double getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(double humidity) {
-        this.humidity = humidity;
-    }
-
-    public int getBattery() {
-        return battery;
-    }
-
-    public void setBattery(int battery) {
-        this.battery = battery;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Instant timestamp) {
-        this.timestamp = timestamp;
-    }
 
     @Override
     public String toString() {
